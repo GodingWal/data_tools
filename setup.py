95fece1d372d5848007fc0b5ffcf300beca47462
GodingWal <GodingWal@users.noreply.github.com>
2020-07-07 07:25:55 -0500

    None

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("../data_GodingWal/README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="GDataTools", # the name that you will install via pip
+    name="GDataTool", # the name that you will install via pip
     version="1.0",
     author="Goding Wal",
     author_email="gwal325@gmail.com",
@@ -12,7 +12,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown", # required if using a md file for long desc
     #license="MIT",
-    url="https://github.com/GodingWal/lambdata_Tools",
+    url="https://github.com/GodingWal/data_tools",
     #keywords="",
     packages=find_packages(), install_requires=['pandas'] # ["Data_Tools"]
 )
